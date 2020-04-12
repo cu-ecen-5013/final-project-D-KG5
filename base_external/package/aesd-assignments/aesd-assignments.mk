6a51c32f7596bddc8aacd28015bb8a5fb4b7a5ff
D-KG5 <dhruvakoley@gmail.com>
2020-04-11 20:17:35 -0600

    fixed .mk file

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,10 +6,10 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 'd15e7f8f3b45f87334cc5137c1a118ed14b42732'
+AESD_ASSIGNMENTS_VERSION = '692aab56684ca89db6545c583f20daa4eb9115fe'
 AESD_ASSIGNMENTS_SITE = 'git@github.com:chwe3468/cu-ecen-5013-final-project-shared.git'
 AESD_ASSIGNMENTS_SITE_METHOD = git
-AESD_ASSIGNMENTS_LDFLAGS = $(LDFLAGS_FOR_BUILD) -lpthread -lrt
+AESD_ASSIGNMENTS_LDFLAGS = $(LDFLAGS_FOR_BUILD) -pthread -lrt
 
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
@@ -20,7 +20,7 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/sensor $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/Dhruva/platform_testing/gettemp.sh $(TARGET_DIR)/usr/bin
 	
-	$(INSTALL) -m 0755 $(@D)/Dhruva/platform_testing/sensor-start-stop.sh $(TARGET_DIR)/etc/init.d/S98sensor
+	$(INSTALL) -m 0755 $(@D)/Dhruva/platform_testing/sensor-start-stop.sh $(TARGET_DIR)/etc/init.d/S60sensor
 endef
 
 
