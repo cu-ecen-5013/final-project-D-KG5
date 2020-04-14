05eec472b2d828a2baacac7ee58fca5b97518364
D-KG5 <dhruvakoley@gmail.com>
2020-04-13 19:47:55 -0600

    sensor package edit upload buildroot changes

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,14 +6,14 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '692aab56684ca89db6545c583f20daa4eb9115fe'
+AESD_ASSIGNMENTS_VERSION = '632cb432cf63814a87467f20345a088a53a06cfb'
 AESD_ASSIGNMENTS_SITE = 'git@github.com:chwe3468/cu-ecen-5013-final-project-shared.git'
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_LDFLAGS = $(LDFLAGS_FOR_BUILD) -pthread -lrt
 
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) LDFLAGS="$(AESD_ASSIGNMENTS_LDFLAGS)" all
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) LDFLAGS="$(AESD_ASSIGNMENTS_LDFLAGS)" sensor
 endef
 
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
